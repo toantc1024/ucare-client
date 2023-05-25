e6effcf17a057961c6e8e70b94eb4849f674d0d9
Toan <thanhnoobs@gmail.com>
2023-05-25 13:06:55 +0700

    merge

--- src/routes/404/pagenotfound.jsx
+++ src/routes/404/pagenotfound.jsx
@@ -1,8 +1,10 @@
-import React from "react";
-import cat from "../../catsleep.png";
+import React, { Fragment } from "react";
+import CatImage from "../../assets/catsleep.png";
+import { useNavigate } from "react-router-dom";
 const PageNotFound = () => {
+  const navigate = useNavigate();
   return (
-    <>
+    <Fragment>
       <div class="w-full h-screen flex flex-col lg:flex-row items-center justify-center space-y-16 lg:space-y-0 space-x-8 2xl:space-x-0">
         <div class="w-full lg:w-1/2 flex flex-col items-center justify-center lg:px-2 xl:px-0 text-center">
           <p class="text-7xl md:text-8xl lg:text-9xl font-bold tracking-wider text-purple-300 mb-5">
@@ -14,14 +16,14 @@ const PageNotFound = () => {
           <p class="text-lg md:text-xl lg:text-2xl text-gray-500 my-12">
             This page is taking a nap somewhere...
           </p>
-          <a
-            href="#"
-            class="flex items-center space-x-2 bg-blue-600 hover:bg-blue-700 text-gray-100 px-4 py-2 rounded transition duration-150"
+          <button
+            onClick={() => navigate("./")}
+            className="flex items-center space-x-2 bg-blue-600 hover:bg-blue-700 text-gray-100 px-4 py-2 rounded transition duration-150"
             title="Return Home"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              class="h-5 w-5"
+              className="h-5 w-5"
               viewBox="0 0 20 20"
               fill="currentColor"
             >
@@ -32,13 +34,13 @@ const PageNotFound = () => {
               ></path>
             </svg>
             <span>Return Home</span>
-          </a>
+          </button>
         </div>
-        <div class="w-1/2 lg:h-full flex lg:items-end justify-center">
-          <img class="lg:h-full object-scale-down" src={cat} />
+        <div className="w-1/2 lg:h-full flex lg:items-end justify-center">
+          <img src={CatImage} alt="" className="lg:h-full object-scale-down" />
         </div>
       </div>
-    </>
+    </Fragment>
   );
 };
 
